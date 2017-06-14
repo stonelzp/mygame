4f7e6f1be6e22a7dd7899d70483f7b278613e5d6
stone <stonelzp1994@gmail.com>
2017-06-14 17:58:53 +0900

    加入了Archer的模型只是当做开始的师姐的模型，只加入了一小部分的模型动画。如果想要继续完善等待整个故事完成之后再完成。下一步则是加入对话的系统。

---
+++ Assets/Scripts/ArcherController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArcherController : MonoBehaviour {
+	private Animator ArcherAnimator;
+
+	// Use this for initialization
+	void Start () {	
+	    ArcherAnimator = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.J)) {
+			ArcherAnimator.SetTrigger ("Attack1");
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			ArcherAnimator.SetTrigger ("Jump");
+		}
+
+		if (Input.GetKeyDown (KeyCode.W)) {
+			ArcherAnimator.SetBool ("Run",true);
+		}
+		if (Input.GetKeyUp (KeyCode.W)) {
+			ArcherAnimator.SetBool("Run",false);
+		}
+	
+	}
+}
