735466b7550adf60f11b3effedebf0238dc32c33
stone <stonelzp1994@gmail.com>
2017-06-19 19:35:15 +0900

    加入了对话框的背景，同时对话的数据也成功的从XML文件中取出来了

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Xml;
+using System.IO;
+public class Dialogue : MonoBehaviour {
+	private string[] dialogue_strings;
+
+
+	// Use this for initialization
+	void Start () {
+		dialogue_init();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	private void dialogue_init(){
+		//dialogue_strings[0] = "";
+		//load xml
+		string filePath=Application.dataPath+"/Resource/Story/dialogue.xml";
+		if (File.Exists (filePath)) {
+			Debug.Log ("Story file is found!");
+			XmlDocument xmldoc = new XmlDocument ();
+			xmldoc.Load (filePath);
+			XmlNodeList node = xmldoc.SelectSingleNode ("dialogue").ChildNodes;
+			foreach (XmlElement nodelist in node) {
+				//get scene-altar dialogue data
+				if (nodelist.Name == "scene-altar") {
+					int i = 0;
+					dialogue_strings=new string[nodelist.ChildNodes.Count*2];
+					foreach (XmlElement dialoguelist in nodelist) {
+						if (dialoguelist.Attributes ["id"].Value != "0") {
+							Debug.Log ("The story is confused!");
+						}
+						dialogue_strings [i] = dialoguelist.SelectSingleNode ("charactor").InnerText;
+						dialogue_strings [i+1] = dialoguelist.SelectSingleNode ("value").InnerText;
+						i += 2;
+					}
+					break;
+				}
+			}
+
+		} else {
+			Debug.Log ("Story file is missing!");
+		}
+
+
+		//show dialogue_string
+		foreach (string i in dialogue_strings) {
+			Debug.Log (i);
+		}
+	}
+}
---
+++ Assets/Scripts/OpacityController.cs
@@ -0,0 +1,17 @@
+﻿//script is attched to GameObject Canvas/DialogueBg 
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;//add UI component reference
+
+public class OpacityController : MonoBehaviour {
+
+	//change the background opacity
+	void Awake () {
+		GetComponent<Image>().color=new Color(1f,1f,1f,0.6f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+}
