af10946827a4fd6c935969331465abf30b787c1c
stone <stonelzp1994@gmail.com>
2017-06-26 16:37:35 +0900

    怪物AI的自动寻路完成30%。

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -1,26 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.AI;
 public class MonsterController : MonoBehaviour {
-    public GameObject AttackTarget;
+    public Transform AttackTarget;
     
 
 
     private Animator MonsterAnimator;
-    private bool isPatrolling = false;
-    private Transform PatrolPosition;
-
+    private bool isPatrolling;
+	//the monster next to Patrol position
+    private Vector3 PatrolPosition;
+	private Vector3 StartPosition;
+	private Vector3 EndPosition;
 
 
 	// Use this for initialization
 	void Start () {
         MonsterAnimator = gameObject.GetComponent<Animator>();
-        PatrolPosition = gameObject.transform;
+		StartPosition = gameObject.transform.position;
+		PatrolPosition=new Vector3 (transform.position.x,transform.position.y,transform.position.z-15.0f);
+		EndPosition = PatrolPosition;
+		gameObject.GetComponent<NavMeshAgent> ().destination = PatrolPosition;
+		isPatrolling = true;
+
 	}
 
     // Update is called once per frame
     void Update() {
-        GetMonsterInput(); 
+        //sGetMonsterInput(); 
+
+		if (isPatrolling) {
+			MonsterPatrol ();
+		}
+
     }
 
     private void GetMonsterInput()
@@ -83,8 +95,33 @@ public class MonsterController : MonoBehaviour {
 
     private void MonsterPatrol()
     {
-
+		if (MonsterIsMoving ()) {
+			MonsterAnimator.SetBool ("Run", true);
+		} else {
+			MonsterAnimator.SetBool ("Run", false);
+			//set the next Patrol place
+			if (Vector3.Distance (EndPosition, PatrolPosition) <= 0.1f) {
+				Debug.Log ("Goal!");
+				PatrolPosition = StartPosition;
+				gameObject.GetComponent<NavMeshAgent> ().destination = PatrolPosition;
+				return;
+			} 
+			if(Vector3.Distance (StartPosition, PatrolPosition) <= 0.1f){
+				PatrolPosition = EndPosition;
+				gameObject.GetComponent<NavMeshAgent> ().destination = PatrolPosition;
+			}
+		}
     }
+
+	private bool MonsterIsMoving(){
+		if (Vector3.Distance (gameObject.transform.position, PatrolPosition) > 0.1f) {
+			return true;
+		} else {
+			Debug.Log ("Turn Round");
+			return false;
+		}
+		
+	}
 
 
 
