eec6bfed55dad5d9d1aadb4d0b4540c20bd65d03
stone <stonelzp1994@gmail.com>
2017-06-29 14:53:42 +0900

    添加了进入传送阵离开的UI，加上跳转功能第一部分序章应该有了大体上的流程。做一次commit。

---
+++ Assets/Scripts/TransportController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TransportController : MonoBehaviour {
+	public GameObject LeaveButton;
+	public GameObject StayButton;
+	//false is stay,true is leave
+	private bool SelectedButton;
+	private Color newColor;
+	// Use this for initialization
+	void Start () {
+		newColor = LeaveButton.GetComponent<Image> ().color;
+		newColor.a = 0.6f;
+		SelectedButton = true;
+		LeaveButton.GetComponent<Image> ().color = newColor;
+		newColor.a = 0.3f;
+		StayButton.GetComponent<Image> ().color = newColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (SelectedButton) {
+			newColor.a = 0.6f;
+			LeaveButton.GetComponent<Image> ().color = newColor;
+			newColor.a = 0.3f;
+			StayButton.GetComponent<Image> ().color = newColor;
+		} else {
+			newColor.a = 0.3f;
+			LeaveButton.GetComponent<Image> ().color = newColor;
+			newColor.a = 0.6f;
+			StayButton.GetComponent<Image> ().color = newColor;
+		}
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			if(!SelectedButton){
+				SelectedButton = true;
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow)){
+			if(SelectedButton){
+				SelectedButton = false;
+			}
+		}
+
+		if(Input.GetKeyDown(KeyCode.Return)){
+			if (SelectedButton) {
+				Debug.Log ("跳转到下一个场景。");
+			} else {
+				Debug.Log ("继续停留。");
+			}
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/TransportTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransportTrigger : MonoBehaviour {
+	public GameObject NPCPlayer;
+	public GameObject Player;
+	public GameObject Particle04;
+	public GameObject Particle05;
+	public GameObject TransportUI;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.tag=="NPC"){
+			NPCPlayer.SetActive (false);
+			if (Particle05.activeSelf) {
+				Particle05.SetActive (false);
+			}
+			Particle05.SetActive (true);
+		}
+		if(other.tag=="Player"){
+			if (!TransportUI.activeSelf) {
+				TransportUI.SetActive (true);
+				Player.GetComponent<PlayerController> ().DialogueAnimationTalkPlayEnd ();//set animation to Normal_Idle
+				Player.GetComponent<PlayerController> ().enabled = false;
+			}
+				
+				
+		}
+	}
+}
